db6aed997c57f4ecf95c73a5e1cc57707e5ebf48
Evgeni Golov <evgeni@golov.de>
2023-10-25 10:44:23 +0200

    make RemoveSatelliteFromNotificationName a noop

--- db/migrate/20160404130619_remove_satellite_from_notification_name.rb
+++ db/migrate/20160404130619_remove_satellite_from_notification_name.rb
@@ -1,26 +1,8 @@
 class RemoveSatelliteFromNotificationName < ActiveRecord::Migration[4.2]
-  class FakeMailNotification < ActiveRecord::Base
-    self.table_name = 'mail_notifications'
-  end
-
   def up
-    FakeMailNotification.all.each do |notification|
-      if notification_names.keys.include?(notification.name)
-        new_name = notification_names[notification.name]
-        FakeMailNotification.where(:name => new_name).destroy_all
-        notification.name = new_name
-        notification.save!
-      end
-    end
-  end
-
-  private
-
-  def notification_names
-    {
-     :satellite_promote_errata => 'promote_errata',
-     :satellote_sync_errata => 'sync_errata',
-     :satellite_host_advisory => 'host_errata_advisory'
-    }.with_indifferent_access
+    # historical placeholder
+    # only required on setups before https://projects.theforeman.org/issues/14459
+    # we can safely assume that fresh installs have the above change already
+    # and existing install had this migration applied back in 2016
   end
 end
