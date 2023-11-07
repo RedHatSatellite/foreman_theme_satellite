8d5e5f9364e86145cc4a84b79214d372e7b36636
Evgeni Golov <evgeni@golov.de>
2023-11-07 16:32:24 +0100

    use supported_icon.png when indicating supported templates (#16)

--- app/helpers/provisioning_templates_helper_branding.rb
+++ app/helpers/provisioning_templates_helper_branding.rb
@@ -4,7 +4,7 @@ module ProvisioningTemplatesHelperBranding
     return super unless ProvisioningTemplatesControllerBranding::SUPPORTED_PROVISIONING_TEMPLATES.include?(name.name)
 
     image_tag(
-      'foreman_theme_satellite/Redhat.png',
+      'foreman_theme_satellite/supported_icon.png',
       class: 'prov-brand-img',
       title: _('Supported by Red Hat')
     ).safe_concat(super)
