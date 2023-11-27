334fba665089ec5f91f7af20df8ea712afcb7847
Evgeni Golov <evgeni@golov.de>
2023-11-27 14:24:07 +0100

    i18n - pulling from tx

---
+++ app/assets/javascripts/foreman_theme_satellite/locale/pt_BR/foreman_theme_satellite.js
@@ -0,0 +1,124 @@
+ locales['foreman_theme_satellite'] = locales['foreman_theme_satellite'] || {}; locales['foreman_theme_satellite']['pt_BR'] = {
+  "domain": "foreman_theme_satellite",
+  "locale_data": {
+    "foreman_theme_satellite": {
+      "": {
+        "Project-Id-Version": "foreman_theme_satellite 1.0.0",
+        "Report-Msgid-Bugs-To": "",
+        "PO-Revision-Date": "2023-11-21 10:43+0000",
+        "Last-Translator": "Bryan Kearney <bryan.kearney@gmail.com>, 2023",
+        "Language-Team": "Portuguese (Brazil) (https://app.transifex.com/foreman/teams/114/pt_BR/)",
+        "MIME-Version": "1.0",
+        "Content-Type": "text/plain; charset=UTF-8",
+        "Content-Transfer-Encoding": "8bit",
+        "Language": "pt_BR",
+        "Plural-Forms": "nplurals=3; plural=(n == 0 || n == 1) ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;",
+        "lang": "pt_BR",
+        "domain": "foreman_theme_satellite",
+        "plural_forms": "nplurals=3; plural=(n == 0 || n == 1) ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;"
+      },
+      "(build: %{version})": [
+        ""
+      ],
+      "API Guide": [
+        ""
+      ],
+      "Administering Red Hat Satellite": [
+        ""
+      ],
+      "Blog": [
+        ""
+      ],
+      "Customer portal": [
+        ""
+      ],
+      "Documentation": [
+        ""
+      ],
+      "Environment only": [
+        "Somente ambiente"
+      ],
+      "Full trace": [
+        ""
+      ],
+      "Host group and Environment": [
+        "Grupo de hosts e ambiente"
+      ],
+      "Host group only": [
+        "Somente grupo de hosts"
+      ],
+      "IRC": [
+        ""
+      ],
+      "If you feel this is an error with Satellite 6 itself, please open a new issue with": [
+        ""
+      ],
+      "Includes %{system} © 2009-%{year} Paul Kelly and Ohad Levy": [
+        ""
+      ],
+      "More information": [
+        ""
+      ],
+      "On August 31, 2022, Red Hat Virtualization entered the Maintenance Support Phase. The Maintenance Support Phase runs until August 31, 2024, followed by the Extended Life Phase, which runs until August 31, 2026.": [
+        ""
+      ],
+      "Operating System": [
+        "Sistema Operacional"
+      ],
+      "Operating system default": [
+        "Sistema operacional padrão"
+      ],
+      "RHEV integration is deprecated.": [
+        ""
+      ],
+      "Red Hat Satellite Blog": [
+        ""
+      ],
+      "Satellite ticketing system": [
+        ""
+      ],
+      "Show unsupported provisioning templates": [
+        ""
+      ],
+      "Show unsupported provisioning templates. When enabled, all the avaiable templates will be shown. When disabled, Red Hat supported templates will be shown only": [
+        ""
+      ],
+      "Support": [
+        ""
+      ],
+      "Supported by Red Hat": [
+        ""
+      ],
+      "The final entry, Operating System default, can be set by editing the %s page.": [
+        "A entrada final, padrão do Sistema Operacional, pode ser definida editando a página %s."
+      ],
+      "Theme changes for Satellite 6.": [
+        ""
+      ],
+      "Transitioning from Red Hat Satellite 5 to Red Hat Satellite 6": [
+        ""
+      ],
+      "Version %{version}": [
+        ""
+      ],
+      "Version %{version} © %{year} Red Hat Inc.": [
+        ""
+      ],
+      "When a Host requests a template (e.g. during provisioning), Foreman selects the optimal match from the available templates of that type, in the following order:": [
+        ""
+      ],
+      "When editing a Template, you must assign a list of Operating Systems with which this Template can be used. Optionally, you can restrict a template to a list of Hostgroups or Environments.": [
+        ""
+      ],
+      "You can find us on %{liberachat} (irc.libera.chat) in #redhat-satellite.": [
+        ""
+      ],
+      "You would probably need to attach the": [
+        ""
+      ],
+      "and relevant log entries.": [
+        ""
+      ]
+    }
+  }
+};
---
+++ app/assets/javascripts/foreman_theme_satellite/locale/ru/foreman_theme_satellite.js
@@ -0,0 +1,124 @@
+ locales['foreman_theme_satellite'] = locales['foreman_theme_satellite'] || {}; locales['foreman_theme_satellite']['ru'] = {
+  "domain": "foreman_theme_satellite",
+  "locale_data": {
+    "foreman_theme_satellite": {
+      "": {
+        "Project-Id-Version": "foreman_theme_satellite 1.0.0",
+        "Report-Msgid-Bugs-To": "",
+        "PO-Revision-Date": "2023-11-21 10:43+0000",
+        "Last-Translator": "Bryan Kearney <bryan.kearney@gmail.com>, 2023",
+        "Language-Team": "Russian (https://app.transifex.com/foreman/teams/114/ru/)",
+        "MIME-Version": "1.0",
+        "Content-Type": "text/plain; charset=UTF-8",
+        "Content-Transfer-Encoding": "8bit",
+        "Language": "ru",
+        "Plural-Forms": "nplurals=4; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<12 || n%100>14) ? 1 : n%10==0 || (n%10>=5 && n%10<=9) || (n%100>=11 && n%100<=14)? 2 : 3);",
+        "lang": "ru",
+        "domain": "foreman_theme_satellite",
+        "plural_forms": "nplurals=4; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<12 || n%100>14) ? 1 : n%10==0 || (n%10>=5 && n%10<=9) || (n%100>=11 && n%100<=14)? 2 : 3);"
+      },
+      "(build: %{version})": [
+        ""
+      ],
+      "API Guide": [
+        ""
+      ],
+      "Administering Red Hat Satellite": [
+        ""
+      ],
+      "Blog": [
+        ""
+      ],
+      "Customer portal": [
+        ""
+      ],
+      "Documentation": [
+        ""
+      ],
+      "Environment only": [
+        "Только окружение"
+      ],
+      "Full trace": [
+        ""
+      ],
+      "Host group and Environment": [
+        "Группа узлов и окружение"
+      ],
+      "Host group only": [
+        "Только группа"
+      ],
+      "IRC": [
+        ""
+      ],
+      "If you feel this is an error with Satellite 6 itself, please open a new issue with": [
+        ""
+      ],
+      "Includes %{system} © 2009-%{year} Paul Kelly and Ohad Levy": [
+        ""
+      ],
+      "More information": [
+        ""
+      ],
+      "On August 31, 2022, Red Hat Virtualization entered the Maintenance Support Phase. The Maintenance Support Phase runs until August 31, 2024, followed by the Extended Life Phase, which runs until August 31, 2026.": [
+        ""
+      ],
+      "Operating System": [
+        "Операционная система"
+      ],
+      "Operating system default": [
+        "Стандартный для операционной системы"
+      ],
+      "RHEV integration is deprecated.": [
+        ""
+      ],
+      "Red Hat Satellite Blog": [
+        ""
+      ],
+      "Satellite ticketing system": [
+        ""
+      ],
+      "Show unsupported provisioning templates": [
+        ""
+      ],
+      "Show unsupported provisioning templates. When enabled, all the avaiable templates will be shown. When disabled, Red Hat supported templates will be shown only": [
+        ""
+      ],
+      "Support": [
+        ""
+      ],
+      "Supported by Red Hat": [
+        ""
+      ],
+      "The final entry, Operating System default, can be set by editing the %s page.": [
+        "Стандартный шаблон для операционной системы можно настроить на странице %s."
+      ],
+      "Theme changes for Satellite 6.": [
+        ""
+      ],
+      "Transitioning from Red Hat Satellite 5 to Red Hat Satellite 6": [
+        ""
+      ],
+      "Version %{version}": [
+        ""
+      ],
+      "Version %{version} © %{year} Red Hat Inc.": [
+        ""
+      ],
+      "When a Host requests a template (e.g. during provisioning), Foreman selects the optimal match from the available templates of that type, in the following order:": [
+        ""
+      ],
+      "When editing a Template, you must assign a list of Operating Systems with which this Template can be used. Optionally, you can restrict a template to a list of Hostgroups or Environments.": [
+        ""
+      ],
+      "You can find us on %{liberachat} (irc.libera.chat) in #redhat-satellite.": [
+        ""
+      ],
+      "You would probably need to attach the": [
+        ""
+      ],
+      "and relevant log entries.": [
+        ""
+      ]
+    }
+  }
+};
