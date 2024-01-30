0ea808ecec33c479ad1af465681458a3741b995e
Evgeni Golov <evgeni@golov.de>
2024-01-30 11:11:26 +0100

    don't disable webpack in tests
    our CI properly compiles webpack before running tests

--- test/test_plugin_helper.rb
+++ test/test_plugin_helper.rb
@@ -5,17 +5,6 @@ require 'test_helper'
 FactoryBot.definition_file_paths << File.join(File.dirname(__FILE__), 'factories')
 FactoryBot.reload
 
-class ActionDispatch::IntegrationTest
-  setup :disable_webpack
-
-  # functional tests will fail if assets are not compiled because page
-  # rendering will try to include the webpack assets path which will throw an
-  # exception.
-  def disable_webpack
-    Webpack::Rails::Manifest.stubs(:asset_paths).returns([])
-  end
-end
-
 def theme_fixture_file(filename)
   ForemanThemeSatellite::Engine.root.join('test', 'fixtures', filename)
 end
