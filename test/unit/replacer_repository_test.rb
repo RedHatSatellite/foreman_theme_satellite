bea416933b41e7737f9535d024a410d7af1a4c18
Thorben Denzer <denzer@atix.de>
2024-07-03 12:26:49 +0200

    Fix outdated branded words: - Red Hat Virtualization: Change from "RHEV" to "RHV" - Red Hat Openstack Platform: Change from "RHEL Openstack Platform" to "Red Hat Openstack Platform" - Fix link to docs for registering hosts using bootstrap script - Simplify regular expressions - Add test based on documentation strings

--- test/unit/replacer_repository_test.rb
+++ test/unit/replacer_repository_test.rb
@@ -14,8 +14,30 @@ class TranslationsTest < ActiveSupport::TestCase
   end
 
   test 'Can work with whitelist branding' do
-    actual = _('HTTP proxy for proxy')
+    actual = _('http proxy for smart proxy')
 
-    assert_equal 'HTTP Proxy for Capsule', actual
+    assert_equal 'HTTP proxy for Capsule', actual
+  end
+
+  test 'random branding test' do
+    # Tests branding based on strings randomly picked from Foreman- and Satellite-documentation
+    test_strings = [
+      'An operating system is a collection of resources that define how Foreman server installs a base operating system on a host.',
+      'Installing the Infoblox CA certificate on Smart Proxy server',
+      'In the Foreman web UI, navigate to Infrastructure > Smart Proxies, select the Smart Proxy with the DHCP Infoblox module, and ensure that the dhcp feature is listed.',
+      'Foreman Server automatically manages the PXE configuration, organizes networking services, and provides the operating system and configuration for the host.',
+      'In Foreman, you can create an HTTP proxy and then assign the HTTP proxy to your EC2 compute resource.',
+    ]
+    translated_strings = [
+      'An operating system is a collection of resources that define how Satellite server installs a base operating system on a host.', # Pr. Hosts: 2.3
+      'Installing the Infoblox CA certificate on Capsule server', # Pr. Hosts: 4.3
+      'In the Satellite web UI, navigate to Infrastructure > Capsules, select the Capsule with the DHCP Infoblox module, and ensure that the dhcp feature is listed.', # Pr. Hosts: 4.4
+      'Satellite Server automatically manages the PXE configuration, organizes networking services, and provides the operating system and configuration for the host.', # Pr. Hosts: 5.3
+      'In Satellite, you can create an HTTP proxy and then assign the HTTP proxy to your EC2 compute resource.', # Pr. Hosts: 14.4
+    ]
+
+    test_strings.zip(translated_strings).each do |original, target|
+      assert_equal target, _(original)
+    end
   end
 end
