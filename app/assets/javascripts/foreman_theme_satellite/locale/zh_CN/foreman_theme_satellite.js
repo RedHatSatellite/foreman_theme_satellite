87b5f0df9c3999c39d53a3f51b07c242bf73ba8f
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-09-19 13:05:59 +0200

    i18n - pulling from tx
    (cherry picked from commit f0c1715c9ecf6c046b63cda5432e7c81e3e16664)

---
+++ app/assets/javascripts/foreman_theme_satellite/locale/zh_CN/foreman_theme_satellite.js
@@ -0,0 +1,124 @@
+ locales['foreman_theme_satellite'] = locales['foreman_theme_satellite'] || {}; locales['foreman_theme_satellite']['zh_CN'] = {
+  "domain": "foreman_theme_satellite",
+  "locale_data": {
+    "foreman_theme_satellite": {
+      "": {
+        "Project-Id-Version": "foreman_theme_satellite 1.0.0",
+        "Report-Msgid-Bugs-To": "",
+        "PO-Revision-Date": "2023-11-21 10:43+0000",
+        "Last-Translator": "Ewoud Kohl van Wijngaarden <ewoud+transifex@kohlvanwijngaarden.nl>, 2024",
+        "Language-Team": "Chinese (China) (https://app.transifex.com/foreman/teams/114/zh_CN/)",
+        "MIME-Version": "1.0",
+        "Content-Type": "text/plain; charset=UTF-8",
+        "Content-Transfer-Encoding": "8bit",
+        "Language": "zh_CN",
+        "Plural-Forms": "nplurals=1; plural=0;",
+        "lang": "zh_CN",
+        "domain": "foreman_theme_satellite",
+        "plural_forms": "nplurals=1; plural=0;"
+      },
+      "(build: %{version})": [
+        "（构建：%{version}）"
+      ],
+      "API Guide": [
+        "API 指南"
+      ],
+      "Administering Red Hat Satellite": [
+        "管理 Red Hat Satellite"
+      ],
+      "Blog": [
+        "部落格"
+      ],
+      "Customer portal": [
+        "客户门户"
+      ],
+      "Documentation": [
+        "文档"
+      ],
+      "Environment only": [
+        "仅环境"
+      ],
+      "Full trace": [
+        "完整追蹤"
+      ],
+      "Host group and Environment": [
+        "主机组和环境"
+      ],
+      "Host group only": [
+        "仅主机组"
+      ],
+      "IRC": [
+        "IRC"
+      ],
+      "If you feel this is an error with Satellite 6 itself, please open a new issue with": [
+        "若您認為這是 Satellite 6 本身的錯誤，請提交錯誤"
+      ],
+      "Includes %{system} © 2009-%{year} Paul Kelly and Ohad Levy": [
+        "包括 %{system} © 2009-%{year} Paul Kelly 和 Ohad Levy"
+      ],
+      "More information": [
+        "更多信息"
+      ],
+      "On August 31, 2022, Red Hat Virtualization entered the Maintenance Support Phase. The Maintenance Support Phase runs until August 31, 2024, followed by the Extended Life Phase, which runs until August 31, 2026.": [
+        "2022 年 8 月 31 日，Red Hat Virtualization 进入维护支持阶段。维护支持阶段持续到 2024 年 8 月 31 日，然后进入延长生命周期阶段，它会持续到 2026 年 8 月 31 日。"
+      ],
+      "Operating System": [
+        "操作系统"
+      ],
+      "Operating system default": [
+        "作業系統預設值"
+      ],
+      "RHEV integration is deprecated.": [
+        "RHEV 集成已弃用。"
+      ],
+      "Red Hat Satellite Blog": [
+        "Red Hat Satellite 部落格"
+      ],
+      "Satellite ticketing system": [
+        "Satellite 的票證系統"
+      ],
+      "Show unsupported provisioning templates": [
+        "显示不被支持的置备模板"
+      ],
+      "Show unsupported provisioning templates. When enabled, all the avaiable templates will be shown. When disabled, Red Hat supported templates will be shown only": [
+        "显示不支持的置备模板。启用后，将显示所有可查询的模板。禁用时，仅显示红帽支持的模板"
+      ],
+      "Support": [
+        "支援"
+      ],
+      "Supported by Red Hat": [
+        "被红帽支持"
+      ],
+      "The final entry, Operating System default, can be set by editing the %s page.": [
+        "最终条目，操作系统默认值，可以通过编辑 %s 页设置。"
+      ],
+      "Theme changes for Satellite 6.": [
+        "Satellite 6 的主题更改。"
+      ],
+      "Transitioning from Red Hat Satellite 5 to Red Hat Satellite 6": [
+        "从 Red Hat Satellite 5 转换到 Red Hat Satellite 6"
+      ],
+      "Version %{version}": [
+        "版本 %{version}"
+      ],
+      "Version %{version} © %{year} Red Hat Inc.": [
+        "版本 %{version} © %{year} Red Hat Inc。"
+      ],
+      "When a Host requests a template (e.g. during provisioning), Foreman selects the optimal match from the available templates of that type, in the following order:": [
+        "当主机请求模板时（例如在置备期间），Foreman 按照以下顺序从该类型的可用模板中选择最佳的匹配项："
+      ],
+      "When editing a Template, you must assign a list of Operating Systems with which this Template can be used. Optionally, you can restrict a template to a list of Hostgroups or Environments.": [
+        "在编辑模板时，您必须分配可搭配此模板使用的一系列操作系统。您也可选择将模板限制到一组主机组或环境。"
+      ],
+      "You can find us on %{liberachat} (irc.libera.chat) in #redhat-satellite.": [
+        "您可以在 #redhat-satellite 的 %{liberachat} (irc.libera.chat) 中找到我们。"
+      ],
+      "You would probably need to attach the": [
+        "您可能需要附加"
+      ],
+      "and relevant log entries.": [
+        "和相關的日誌項目。"
+      ]
+    }
+  }
+};
