ed7202279eee10ea947f499aeec6c54cd466eda5
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-09-19 13:05:59 +0200

    i18n - pulling from tx
    (cherry picked from commit f0c1715c9ecf6c046b63cda5432e7c81e3e16664)

--- app/assets/javascripts/foreman_theme_satellite/locale/ka/foreman_theme_satellite.js
+++ app/assets/javascripts/foreman_theme_satellite/locale/ka/foreman_theme_satellite.js
@@ -3,7 +3,7 @@
   "locale_data": {
     "foreman_theme_satellite": {
       "": {
-        "Project-Id-Version": "foreman_theme_satellite 13.2.0",
+        "Project-Id-Version": "foreman_theme_satellite 14.0.1",
         "Report-Msgid-Bugs-To": "",
         "PO-Revision-Date": "2023-11-21 10:43+0000",
         "Last-Translator": "Temuri Doghonadze <temuri.doghonadze@gmail.com>, 2023",
@@ -17,47 +17,17 @@
         "domain": "foreman_theme_satellite",
         "plural_forms": "nplurals=2; plural=(n!=1);"
       },
-      "Supported by Red Hat": [
-        "მხარდაჭერილია Red Hat-ის მიერ"
+      "(build: %{version})": [
+        "(აგება: %{version})"
       ],
-      "RHEV integration is deprecated.": [
-        "RHEV-ის ჩაშენება მოძველებულია."
+      "API Guide": [
+        "API-ის გიდი"
       ],
-      "On August 31, 2022, Red Hat Virtualization entered the Maintenance Support Phase. The Maintenance Support Phase runs until August 31, 2024, followed by the Extended Life Phase, which runs until August 31, 2026.": [
-        "2022 წლის 31 აგვისტოს RHEV რემონტის მხარდაჭერის ფაზაზე გადავიდა. ის 2024 წლის 31 აგვისტომდე გაგრძელდება, რომელიც გაფართოებული სიცოცხლის ფაზით გაგრძელდება, 2026 წლის 31 აგვისტომდე."
+      "Administering Red Hat Satellite": [
+        "Red Hat Satellite-ის ადმინისტრირება"
       ],
-      "More information": [
-        "მეტი ინფორმაცია"
-      ],
-      "When editing a Template, you must assign a list of Operating Systems with which this Template can be used. Optionally, you can restrict a template to a list of Hostgroups or Environments.": [
-        "შაბლონის რედაქტირებისას თქვენ უნდა მიანიჭოთ სია ოპერაციული სისტემებისა, რომლებთანაც შესაძლებელია ამ შაბლონის გამოყენება. სურვილისამებრ, შეგიძლიათ, შაბლონი ჰოსტების ჯგუფების სიით ან/და გარემოებით შეზღუდოთ."
-      ],
-      "When a Host requests a template (e.g. during provisioning), Foreman selects the optimal match from the available templates of that type, in the following order:": [
-        "როდესაც ჰოსტი მოითხოვს შაბლონს (მაგ. სამუშაოდ მომზადების დროს), Foreman საუკეთესო შესატყვისს ამ ტიპის არსებული შაბლონებიდან შემდეგი თანამიმდევრობით შეარჩევს:"
-      ],
-      "Host group and Environment": [
-        "ჰოსტების ჯგუფი და გარემო"
-      ],
-      "Host group only": [
-        "მხოლოდ ჰოსტების ჯგუფი"
-      ],
-      "Environment only": [
-        "მხოლოდ გარემო"
-      ],
-      "Operating system default": [
-        "ოპერაციული სისტემის ნაგულისხმები მნიშვნელობა"
-      ],
-      "The final entry, Operating System default, can be set by editing the %s page.": [
-        "საბოლოო ჩანაწერი, ოპერაციული სისტემის ნაგულისხმები, შეიძლება დაყენდეს %s გვერდის რედაქტირებით."
-      ],
-      "Operating System": [
-        "ოპერაციული სისტემა"
-      ],
-      "Version %{version}": [
-        "ვერსია %{version}"
-      ],
-      "Support": [
-        "მხარდაჭერა"
+      "Blog": [
+        "ბლოგი"
       ],
       "Customer portal": [
         "მომხმარებლის პორტალი"
@@ -65,59 +35,89 @@
       "Documentation": [
         "დოკუმენტაცია"
       ],
-      "Administering Red Hat Satellite": [
-        "Red Hat Satellite-ის ადმინისტრირება"
-      ],
-      "Transitioning from Red Hat Satellite 5 to Red Hat Satellite 6": [
-        "გადასვლა Red Hat Satellite 5-დან to Red Hat Satellite 6-ზე"
-      ],
-      "API Guide": [
-        "API-ის გიდი"
-      ],
-      "Blog": [
-        "ბლოგი"
-      ],
-      "Red Hat Satellite Blog": [
-        "Red Hat Satellite-ის ბლოგი"
-      ],
-      "IRC": [
-        "IRC"
-      ],
-      "You can find us on %{liberachat} (irc.libera.chat) in #redhat-satellite.": [
-        "%{liberachat}-ზე (irc.libera.chat) ჩვენი პოვნა #redhat-satellite არხზე შეგიძლიათ."
-      ],
-      "Version %{version} © %{year} Red Hat Inc.": [
-        "ვერსია %{version} © %{year} Red Hat Inc."
-      ],
-      "Includes %{system} © 2009-%{year} Paul Kelly and Ohad Levy": [
-        "შეიცავს %{system} © 2009-%{year} პოლ კელი და ოჰედ ლევი"
-      ],
-      "If you feel this is an error with Satellite 6 itself, please open a new issue with": [
-        "თუ გგონიათ, რომ ეს შეცდომა Satellite 6-ის შეცდომაა, გახსენით ახალი ბილეთი"
-      ],
-      "Satellite ticketing system": [
-        "Satellite-ის ბილეთებს სისტემა"
-      ],
-      "You would probably need to attach the": [
-        "ალბათ, დაგჭირდებათ მიამაგროთ"
+      "Environment only": [
+        "მხოლოდ გარემო"
       ],
       "Full trace": [
         "სრული ტრეისი"
       ],
-      "and relevant log entries.": [
-        "და შესაბამისი ჟურნალის ჩანაწერები."
+      "Host group and Environment": [
+        "ჰოსტების ჯგუფი და გარემო"
       ],
-      "(build: %{version})": [
-        "(აგება: %{version})"
+      "Host group only": [
+        "მხოლოდ ჰოსტების ჯგუფი"
       ],
-      "Show unsupported provisioning templates. When enabled, all the avaiable templates will be shown. When disabled, Red Hat supported templates will be shown only": [
-        "მხარდაუჭერელი გაშლის ნიმუშების ჩვენება. როცა ჩართულია, ნაჩვენები იქნება ყველა ხელმისაწვდომი ნიმუში. როცა გამორთულია, ნაჩვენები იქნება, მხოლოდ, Red Hat-ის მიერ მხარდაჭერილი ნმუშები"
+      "IRC": [
+        "IRC"
+      ],
+      "If you feel this is an error with Satellite 6 itself, please open a new issue with": [
+        "თუ გგონიათ, რომ ეს შეცდომა Satellite 6-ის შეცდომაა, გახსენით ახალი ბილეთი"
+      ],
+      "Includes %{system} © 2009-%{year} Paul Kelly and Ohad Levy": [
+        "შეიცავს %{system} © 2009-%{year} პოლ კელი და ოჰედ ლევი"
+      ],
+      "More information": [
+        "მეტი ინფორმაცია"
+      ],
+      "On August 31, 2022, Red Hat Virtualization entered the Maintenance Support Phase. The Maintenance Support Phase runs until August 31, 2024, followed by the Extended Life Phase, which runs until August 31, 2026.": [
+        "2022 წლის 31 აგვისტოს RHEV რემონტის მხარდაჭერის ფაზაზე გადავიდა. ის 2024 წლის 31 აგვისტომდე გაგრძელდება, რომელიც გაფართოებული სიცოცხლის ფაზით გაგრძელდება, 2026 წლის 31 აგვისტომდე."
+      ],
+      "Operating System": [
+        "ოპერაციული სისტემა"
+      ],
+      "Operating system default": [
+        "ოპერაციული სისტემის ნაგულისხმები მნიშვნელობა"
+      ],
+      "RHEV integration is deprecated.": [
+        "RHEV-ის ჩაშენება მოძველებულია."
+      ],
+      "Red Hat Satellite Blog": [
+        "Red Hat Satellite-ის ბლოგი"
+      ],
+      "Satellite ticketing system": [
+        "Satellite-ის ბილეთებს სისტემა"
       ],
       "Show unsupported provisioning templates": [
         "მხარდაუჭერელი გაშლის ნიმუშების ჩვენება"
       ],
+      "Show unsupported provisioning templates. When enabled, all the avaiable templates will be shown. When disabled, Red Hat supported templates will be shown only": [
+        "მხარდაუჭერელი გაშლის ნიმუშების ჩვენება. როცა ჩართულია, ნაჩვენები იქნება ყველა ხელმისაწვდომი ნიმუში. როცა გამორთულია, ნაჩვენები იქნება, მხოლოდ, Red Hat-ის მიერ მხარდაჭერილი ნმუშები"
+      ],
+      "Support": [
+        "მხარდაჭერა"
+      ],
+      "Supported by Red Hat": [
+        "მხარდაჭერილია Red Hat-ის მიერ"
+      ],
+      "The final entry, Operating System default, can be set by editing the %s page.": [
+        "საბოლოო ჩანაწერი, ოპერაციული სისტემის ნაგულისხმები, შეიძლება დაყენდეს %s გვერდის რედაქტირებით."
+      ],
       "Theme changes for Satellite 6.": [
         "თემის ცვლილებები Satellite 6-სთვის."
+      ],
+      "Transitioning from Red Hat Satellite 5 to Red Hat Satellite 6": [
+        "გადასვლა Red Hat Satellite 5-დან to Red Hat Satellite 6-ზე"
+      ],
+      "Version %{version}": [
+        "ვერსია %{version}"
+      ],
+      "Version %{version} © %{year} Red Hat Inc.": [
+        "ვერსია %{version} © %{year} Red Hat Inc."
+      ],
+      "When a Host requests a template (e.g. during provisioning), Foreman selects the optimal match from the available templates of that type, in the following order:": [
+        "როდესაც ჰოსტი მოითხოვს შაბლონს (მაგ. სამუშაოდ მომზადების დროს), Foreman საუკეთესო შესატყვისს ამ ტიპის არსებული შაბლონებიდან შემდეგი თანამიმდევრობით შეარჩევს:"
+      ],
+      "When editing a Template, you must assign a list of Operating Systems with which this Template can be used. Optionally, you can restrict a template to a list of Hostgroups or Environments.": [
+        "შაბლონის რედაქტირებისას თქვენ უნდა მიანიჭოთ სია ოპერაციული სისტემებისა, რომლებთანაც შესაძლებელია ამ შაბლონის გამოყენება. სურვილისამებრ, შეგიძლიათ, შაბლონი ჰოსტების ჯგუფების სიით ან/და გარემოებით შეზღუდოთ."
+      ],
+      "You can find us on %{liberachat} (irc.libera.chat) in #redhat-satellite.": [
+        "%{liberachat}-ზე (irc.libera.chat) ჩვენი პოვნა #redhat-satellite არხზე შეგიძლიათ."
+      ],
+      "You would probably need to attach the": [
+        "ალბათ, დაგჭირდებათ მიამაგროთ"
+      ],
+      "and relevant log entries.": [
+        "და შესაბამისი ჟურნალის ჩანაწერები."
       ]
     }
   }
