f0c1715c9ecf6c046b63cda5432e7c81e3e16664
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-09-19 13:05:59 +0200

    i18n - pulling from tx

---
+++ app/assets/javascripts/foreman_theme_satellite/locale/es/foreman_theme_satellite.js
@@ -0,0 +1,124 @@
+ locales['foreman_theme_satellite'] = locales['foreman_theme_satellite'] || {}; locales['foreman_theme_satellite']['es'] = {
+  "domain": "foreman_theme_satellite",
+  "locale_data": {
+    "foreman_theme_satellite": {
+      "": {
+        "Project-Id-Version": "foreman_theme_satellite 1.0.0",
+        "Report-Msgid-Bugs-To": "",
+        "PO-Revision-Date": "2023-11-21 10:43+0000",
+        "Last-Translator": "Ewoud Kohl van Wijngaarden <ewoud+transifex@kohlvanwijngaarden.nl>, 2024",
+        "Language-Team": "Spanish (https://app.transifex.com/foreman/teams/114/es/)",
+        "MIME-Version": "1.0",
+        "Content-Type": "text/plain; charset=UTF-8",
+        "Content-Transfer-Encoding": "8bit",
+        "Language": "es",
+        "Plural-Forms": "nplurals=3; plural=n == 1 ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;",
+        "lang": "es",
+        "domain": "foreman_theme_satellite",
+        "plural_forms": "nplurals=3; plural=n == 1 ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;"
+      },
+      "(build: %{version})": [
+        ""
+      ],
+      "API Guide": [
+        "Guía API"
+      ],
+      "Administering Red Hat Satellite": [
+        ""
+      ],
+      "Blog": [
+        "Diario web"
+      ],
+      "Customer portal": [
+        "Portal del cliente"
+      ],
+      "Documentation": [
+        "Documentación"
+      ],
+      "Environment only": [
+        "Solo entorno"
+      ],
+      "Full trace": [
+        "Mensaje completo"
+      ],
+      "Host group and Environment": [
+        "Grupo del host y Entorno"
+      ],
+      "Host group only": [
+        "Solo Grupo del host"
+      ],
+      "IRC": [
+        "IRC"
+      ],
+      "If you feel this is an error with Satellite 6 itself, please open a new issue with": [
+        "Si cree que esto es un error de Satellite 6, por favor, reporte una incidencia con"
+      ],
+      "Includes %{system} © 2009-%{year} Paul Kelly and Ohad Levy": [
+        ""
+      ],
+      "More information": [
+        ""
+      ],
+      "On August 31, 2022, Red Hat Virtualization entered the Maintenance Support Phase. The Maintenance Support Phase runs until August 31, 2024, followed by the Extended Life Phase, which runs until August 31, 2026.": [
+        ""
+      ],
+      "Operating System": [
+        "Sistema operativo"
+      ],
+      "Operating system default": [
+        "Sistema operativo predeterminado"
+      ],
+      "RHEV integration is deprecated.": [
+        ""
+      ],
+      "Red Hat Satellite Blog": [
+        "Red Hat Satellite Blog"
+      ],
+      "Satellite ticketing system": [
+        "Sistema de tiquetes Satellite "
+      ],
+      "Show unsupported provisioning templates": [
+        ""
+      ],
+      "Show unsupported provisioning templates. When enabled, all the avaiable templates will be shown. When disabled, Red Hat supported templates will be shown only": [
+        ""
+      ],
+      "Support": [
+        "Soporte"
+      ],
+      "Supported by Red Hat": [
+        ""
+      ],
+      "The final entry, Operating System default, can be set by editing the %s page.": [
+        "La última entrada, sistema operativo predeterminado, puede configurarse al editar la página %s."
+      ],
+      "Theme changes for Satellite 6.": [
+        "Cambios de tema para Satellite 6."
+      ],
+      "Transitioning from Red Hat Satellite 5 to Red Hat Satellite 6": [
+        ""
+      ],
+      "Version %{version}": [
+        ""
+      ],
+      "Version %{version} © %{year} Red Hat Inc.": [
+        ""
+      ],
+      "When a Host requests a template (e.g. during provisioning), Foreman selects the optimal match from the available templates of that type, in the following order:": [
+        "Cuando un host solicita una plantilla (p. ej., durante el aprovisionamiento), Foreman selecciona la mejor coincidencia en las plantillas disponibles de ese tipo, en el siguiente orden:"
+      ],
+      "When editing a Template, you must assign a list of Operating Systems with which this Template can be used. Optionally, you can restrict a template to a list of Hostgroups or Environments.": [
+        "Cuando edita una plantilla, debe asignar una lista de sistemas operativos con los que esta plantilla puede usarse. De manera opcional, puede restringir una plantilla a una lista de grupos de hosts o entornos."
+      ],
+      "You can find us on %{liberachat} (irc.libera.chat) in #redhat-satellite.": [
+        ""
+      ],
+      "You would probably need to attach the": [
+        "Probablemente sea necesario añadir el"
+      ],
+      "and relevant log entries.": [
+        "y entradas relevantes de registros"
+      ]
+    }
+  }
+};
---
+++ app/assets/javascripts/foreman_theme_satellite/locale/ja/foreman_theme_satellite.js
@@ -0,0 +1,124 @@
+ locales['foreman_theme_satellite'] = locales['foreman_theme_satellite'] || {}; locales['foreman_theme_satellite']['ja'] = {
+  "domain": "foreman_theme_satellite",
+  "locale_data": {
+    "foreman_theme_satellite": {
+      "": {
+        "Project-Id-Version": "foreman_theme_satellite 1.0.0",
+        "Report-Msgid-Bugs-To": "",
+        "PO-Revision-Date": "2023-11-21 10:43+0000",
+        "Last-Translator": "Ewoud Kohl van Wijngaarden <ewoud+transifex@kohlvanwijngaarden.nl>, 2024",
+        "Language-Team": "Japanese (https://app.transifex.com/foreman/teams/114/ja/)",
+        "MIME-Version": "1.0",
+        "Content-Type": "text/plain; charset=UTF-8",
+        "Content-Transfer-Encoding": "8bit",
+        "Language": "ja",
+        "Plural-Forms": "nplurals=1; plural=0;",
+        "lang": "ja",
+        "domain": "foreman_theme_satellite",
+        "plural_forms": "nplurals=1; plural=0;"
+      },
+      "(build: %{version})": [
+        "(ビルド: %{version})"
+      ],
+      "API Guide": [
+        "API ガイド"
+      ],
+      "Administering Red Hat Satellite": [
+        "Red Hat Satellite の管理"
+      ],
+      "Blog": [
+        "ブログ"
+      ],
+      "Customer portal": [
+        "カスタマーポータル"
+      ],
+      "Documentation": [
+        "ドキュメント"
+      ],
+      "Environment only": [
+        "環境のみ"
+      ],
+      "Full trace": [
+        "完全トレース"
+      ],
+      "Host group and Environment": [
+        "ホストグループと環境"
+      ],
+      "Host group only": [
+        "ホストグループのみ"
+      ],
+      "IRC": [
+        "IRC"
+      ],
+      "If you feel this is an error with Satellite 6 itself, please open a new issue with": [
+        "これが Satellite 6 自体のエラーであると思われる場合は、新規の問題として以下に提出してください:"
+      ],
+      "Includes %{system} © 2009-%{year} Paul Kelly and Ohad Levy": [
+        "%{system} (© 2009-%{year} Paul Kelly and Ohad Levy) を含む"
+      ],
+      "More information": [
+        "詳細"
+      ],
+      "On August 31, 2022, Red Hat Virtualization entered the Maintenance Support Phase. The Maintenance Support Phase runs until August 31, 2024, followed by the Extended Life Phase, which runs until August 31, 2026.": [
+        "2022 年 8 月 31 日、Red Hat Virtualization はメンテナンスサポートフェーズに入りました。メンテナンスサポートフェーズは 2024 年 8 月 31 日まで続きます。その後、延長ライフフェーズが 2026 年 8 月 31 日まで続きます。"
+      ],
+      "Operating System": [
+        "オペレーティングシステム"
+      ],
+      "Operating system default": [
+        "オペレーティングシステムのデフォルト"
+      ],
+      "RHEV integration is deprecated.": [
+        "RHEV 統合は非推奨です。"
+      ],
+      "Red Hat Satellite Blog": [
+        "Red Hat Satellite ブログ"
+      ],
+      "Satellite ticketing system": [
+        "Satellite チケットシステム"
+      ],
+      "Show unsupported provisioning templates": [
+        "サポートされていないプロビジョニングテンプレートの表示"
+      ],
+      "Show unsupported provisioning templates. When enabled, all the avaiable templates will be shown. When disabled, Red Hat supported templates will be shown only": [
+        "サポートされていないプロビジョニングテンプレートを表示します。有効にすると、使用可能なすべてのテンプレートが表示されます。無効にすると、Red Hat がサポートするテンプレートだけが表示されます"
+      ],
+      "Support": [
+        "サポート"
+      ],
+      "Supported by Red Hat": [
+        "Red Hat によるサポート"
+      ],
+      "The final entry, Operating System default, can be set by editing the %s page.": [
+        "最終エントリーのオペレーティングシステムのデフォルトは、%s ページを編集して設定できます。"
+      ],
+      "Theme changes for Satellite 6.": [
+        "Satellite 6 向けテーマの変更"
+      ],
+      "Transitioning from Red Hat Satellite 5 to Red Hat Satellite 6": [
+        "Red Hat Satellite 5 から Red Hat Satellite 6 への移行"
+      ],
+      "Version %{version}": [
+        "バージョン %{version}"
+      ],
+      "Version %{version} © %{year} Red Hat Inc.": [
+        "バージョン %{version} © %{year} Red Hat Inc."
+      ],
+      "When a Host requests a template (e.g. during provisioning), Foreman selects the optimal match from the available templates of that type, in the following order:": [
+        "ホストがテンプレートを要求する際 (例: プロビジョニング時) に、Foreman は以下の順序で、該当タイプの利用可能なテンプレートから最適な候補を選択します。"
+      ],
+      "When editing a Template, you must assign a list of Operating Systems with which this Template can be used. Optionally, you can restrict a template to a list of Hostgroups or Environments.": [
+        "テンプレートの編集時に、このテンプレートを使用できるオペレーティングシステムの一覧を割り当てる必要があります。オプションで、テンプレートをホストグループまたは環境の一覧に制限することができます。"
+      ],
+      "You can find us on %{liberachat} (irc.libera.chat) in #redhat-satellite.": [
+        "%{liberachat} (irc.libera.chat) の #redhat-satellite からお問い合わせいただけます。"
+      ],
+      "You would probably need to attach the": [
+        "必要な添付内容:"
+      ],
+      "and relevant log entries.": [
+        "と関連するログエントリー。"
+      ]
+    }
+  }
+};
