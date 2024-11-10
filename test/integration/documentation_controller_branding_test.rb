55ccd3298d9d799261b83b134291bcba3fd990fb
Shim Shtein <sshtein@redhat.com>
2024-11-10 09:16:28 -0500

    Fix failing unit test

--- test/integration/documentation_controller_branding_test.rb
+++ test/integration/documentation_controller_branding_test.rb
@@ -4,7 +4,7 @@ class DocumentationControllerBrandingTest < ActionDispatch::IntegrationTest
   def test_docs_redirect_branded
     get "/links/docs/Managing_Hosts?chapter=registering-a-host_managing-hosts"
 
-    assert_redirected_to "https://docs.redhat.com/documentation/en-us/red_hat_satellite/#{ForemanThemeSatellite.documentation_version}/html/managing_hosts/registering_hosts_to_server_managing-hosts#Registering_Hosts_by_Using_Global_Registration_managing-hosts"
+    assert_redirected_to "https://docs.redhat.com/documentation/en-us/red_hat_satellite/#{ForemanThemeSatellite.documentation_version}/html/managing_hosts/registering-hosts-and-setting-up-host-integration_managing-hosts#Registering_Hosts_by_Using_Global_Registration_managing-hosts"
   end
 
   def test_docs_redirect_unknown_chapter
