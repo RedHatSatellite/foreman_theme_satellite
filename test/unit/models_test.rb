247bf3848d73477ea30fb1039485a218830dc4f5
Arvind Jangir <ajangir@ajangir-thinkpadt14gen5.punetw6.csb>
2026-01-12 12:59:10 +0530

    Add rule for kubevirt and remove tests for kubevirt

--- test/unit/models_test.rb
+++ test/unit/models_test.rb
@@ -12,8 +12,4 @@ class ModelsTest < ActiveSupport::TestCase
   test "check Setting.replace_keywords uses Satellite version" do
     assert_equal ForemanThemeSatellite::SATELLITE_VERSION, Setting.replace_keywords('$VERSION')
   end
-
-  test "check kubevirt friendly name" do
-    assert_equal ::ForemanKubevirt::Kubevirt::provider_friendly_name, "OpenShift Virtualization", "Friendly name override was unsuccessful"
-  end
 end
