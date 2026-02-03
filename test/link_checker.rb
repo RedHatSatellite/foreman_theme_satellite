4920edc409dfbada7b8510fc4d8bc2cc0b482138
Adam Ruzicka <aruzicka@redhat.com>
2026-02-03 15:59:24 +0100

    "Fix" tests

--- test/link_checker.rb
+++ test/link_checker.rb
@@ -26,21 +26,16 @@ class LinksChecker
 
   private
 
-  # Will decompose URL from https://localhost:3000/documentation/en-us/red_hat_satellite/6.15/html-single/managing_configurations_using_ansible_integration_in_red_hat_satellite/index#Importing_Ansible_Roles_and_Variables_ansible
+  # Will decompose URL from managing_configurations_using_ansible_integration_in_red_hat_satellite/index#Importing_Ansible_Roles_and_Variables_ansible
   # to two parts:
   # path: managing_configurations_using_ansible_integration_in_red_hat_satellite
   # chapter: Importing_Ansible_Roles_and_Variables_ansible
   def decompose_link(link)
     uri = URI.parse(link)
 
-    doc_path = extract_doc_path(uri.path)
+    doc_path = uri.path.sub('/index', '')
     anchor = uri.fragment
 
     [doc_path, anchor]
-  end
-
-  def extract_doc_path(path)
-    # take the right part of the path after the '/html-single/' from the original link and remove the trailing '/index'
-    path.split('/html-single/', 2)[1].sub('/index', '')
   end
 end
