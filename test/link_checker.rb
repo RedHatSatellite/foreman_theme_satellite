800838a9862c7dca4bd46abd10680c9607195b3d
Adam Ruzicka <aruzicka@redhat.com>
2026-03-05 18:58:51 +0100

    Make link checker accept auxiliary IDs
    https://github.com/theforeman/foreman-documentation/pull/4693

--- test/link_checker.rb
+++ test/link_checker.rb
@@ -47,6 +47,19 @@ class LinksChecker
 
     inner_hash = hash[first]
 
+    # The structure looks like this:
+    # { "guide1": { "chapter1": ["anchor1", "anchor2", "auxid1"] } }
+    # In this case, auxid1 is an auxiliary ID for chapter1
+    #
+    # If we haven't found a direct match, we need to look into the values to
+    # see if we can get a match by the auxiliary id. The auxiliary IDs are
+    # case-insensitive, hence the downcases
+    # The auxiliary ID lookup is only available at the chapter-anchor level
+    if inner_hash.nil? && hash.values.all? { |v| v.kind_of? Array }
+      key, _ = hash.find { |_k, v| v.map(&:downcase).include? first.downcase }
+      inner_hash = hash[key]
+    end
+
     # rubocop:disable Rails/Blank
     return inner_hash if rest.nil? || rest.empty?
     # rubocop:enable Rails/Blank
