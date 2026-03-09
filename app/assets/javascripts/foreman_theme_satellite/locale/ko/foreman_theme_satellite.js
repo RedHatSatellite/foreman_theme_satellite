05ad99c01c2cf3aea4e53c4d24a80d0ac9266250
alazik <alazik@redhat.com>
2026-03-09 13:10:29 +0000

    i18n - pulling from tx

--- app/assets/javascripts/foreman_theme_satellite/locale/ko/foreman_theme_satellite.js
+++ app/assets/javascripts/foreman_theme_satellite/locale/ko/foreman_theme_satellite.js
@@ -3,7 +3,7 @@
   "locale_data": {
     "foreman_theme_satellite": {
       "": {
-        "Project-Id-Version": "foreman_theme_satellite 15.3.1",
+        "Project-Id-Version": "foreman_theme_satellite 16.2.3",
         "Report-Msgid-Bugs-To": "",
         "PO-Revision-Date": "2023-11-21 10:43+0000",
         "Last-Translator": "Ondřej Gajdušek, 2025",
@@ -34,6 +34,9 @@
       ],
       "Customer portal": [
         "고객 포털"
+      ],
+      "Defaults to Red Hat online documentation. Override this setting to your Red Hat Offline Knowledge Portal instance URL for offline documentation support. Supports only HTTP and HTTPS schemes.": [
+        ""
       ],
       "Documentation": [
         "문서 "
@@ -70,6 +73,9 @@
       ],
       "Red Hat Satellite Blog": [
         "Red Hat Satellite 블로그"
+      ],
+      "Red Hat documentation server URL": [
+        ""
       ],
       "Satellite ticketing system": [
         "Satellite 티켓팅 시스템"
--- app/assets/javascripts/foreman_theme_satellite/locale/zh_CN/foreman_theme_satellite.js
+++ app/assets/javascripts/foreman_theme_satellite/locale/zh_CN/foreman_theme_satellite.js
@@ -3,7 +3,7 @@
   "locale_data": {
     "foreman_theme_satellite": {
       "": {
-        "Project-Id-Version": "foreman_theme_satellite 15.3.1",
+        "Project-Id-Version": "foreman_theme_satellite 16.2.3",
         "Report-Msgid-Bugs-To": "",
         "PO-Revision-Date": "2023-11-21 10:43+0000",
         "Last-Translator": "Ondřej Gajdušek, 2025",
@@ -34,6 +34,9 @@
       ],
       "Customer portal": [
         "客户门户"
+      ],
+      "Defaults to Red Hat online documentation. Override this setting to your Red Hat Offline Knowledge Portal instance URL for offline documentation support. Supports only HTTP and HTTPS schemes.": [
+        ""
       ],
       "Documentation": [
         "文档"
@@ -70,6 +73,9 @@
       ],
       "Red Hat Satellite Blog": [
         "Red Hat Satellite 部落格"
+      ],
+      "Red Hat documentation server URL": [
+        ""
       ],
       "Satellite ticketing system": [
         "Satellite 的票證系統"
