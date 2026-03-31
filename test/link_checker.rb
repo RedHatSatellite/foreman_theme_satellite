d5a6e925e757dcc7d2b4cf7c42510035e0bf101e
Adam Růžička <adamruzicka@users.noreply.github.com>
2026-03-31 16:24:26 +0200

    Make link checker accept auxiliary IDs (#320)
    * Make link checker accept auxiliary IDs
    
    * Make all matching case-insensitive
    
    * Accept aliases in a separate file
    
    * Drop configured redirect checking
    
    * Address review comments

--- test/link_checker.rb
+++ test/link_checker.rb
@@ -2,8 +2,12 @@ require 'json'
 require 'uri'
 
 class LinksChecker
-  def initialize(toc:)
+  def initialize(toc:, aliases: nil)
     @toc = JSON.parse(File.read(toc))
+    @aliases = {}
+
+    return unless aliases && File.exist?(aliases)
+    @aliases = JSON.parse(File.read(aliases)).to_h { |k, vs| [k.downcase, vs.map(&:downcase)] }
   end
 
   def test_link(url)
@@ -11,7 +15,12 @@ class LinksChecker
 
     return false unless doc_path
 
-    chapters = @toc[doc_path]
+    chapters = case_insensitive_lookup(@toc, doc_path)
+    if chapters.nil?
+      doc_path = guide_alias(doc_path)
+      return false if doc_path.nil?
+      chapters = case_insensitive_lookup(@toc, doc_path)
+    end
 
     return false if chapters.nil? # no such page
     return true if anchor.nil? # the page exists, no particular anchor means we want just the page
@@ -19,9 +28,9 @@ class LinksChecker
     # Even though the ToC generated with html-multi in mind,
     # we can't properly dig due to html-single using anchors on index page for the whole section
     # dropping the intermediate path
-    page = chapters.keys.map(&:downcase) + chapters.values.flatten.map(&:downcase)
+    pages = chapters.keys.map(&:downcase) + chapters.values.flatten.map(&:downcase)
 
-    page.include?(anchor.downcase)
+    pages.include?(anchor.downcase) || pages.include?(guide_anchor_aliases(doc_path, anchor))
   end
 
   private
@@ -37,5 +46,29 @@ class LinksChecker
     anchor = uri.fragment
 
     [doc_path, anchor]
+  end
+
+  def guide_alias(guide)
+    k, _v = @aliases.find { |_k, vs| vs.include? guide.downcase }
+
+    return if k.nil?
+
+    k
+  end
+
+  def guide_anchor_aliases(guide, anchor)
+    k, _v = @aliases.find do |_k, aliases|
+      re = %r{^#{Regexp.escape(guide)}/.*##{Regexp.escape(anchor)}$}
+      aliases.any? { |a| a =~ re }
+    end
+
+    k&.split('#')&.last
+  end
+
+  def case_insensitive_lookup(hash, key)
+    return hash[key] if hash[key]
+
+    _, v = hash.find { |k, _| k.casecmp(key).zero? }
+    v
   end
 end
