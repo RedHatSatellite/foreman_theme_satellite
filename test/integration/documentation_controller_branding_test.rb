79fed32bbf3c4218b9192e0168693c9264230c86
nofaralfasi <nofaralfasi@gmail.com>
2026-05-31 21:47:19 +0300

    Map Inventory Upload docs from Managing_Hosts as well
    Keep the Administering_Project mapping and add a compatibility key
    under Managing_Hosts so older rh_cloud packages still reach the
    administering cloud-connection section.

--- test/integration/documentation_controller_branding_test.rb
+++ test/integration/documentation_controller_branding_test.rb
@@ -13,6 +13,12 @@ class DocumentationControllerBrandingTest < ActionDispatch::IntegrationTest
     assert_redirected_to "https://docs.redhat.com/documentation/en-us/red_hat_satellite/#{ForemanThemeSatellite.documentation_version}/html-single/administering_red_hat_satellite/index#configuring-satellite-server-for-cloud-connection"
   end
 
+  def test_docs_redirect_inventory_upload_documentation_managing_hosts_compat
+    get "/links/docs/Managing_Hosts?chapter=configuring-foreman-server-for-cloud-connection"
+
+    assert_redirected_to "https://docs.redhat.com/documentation/en-us/red_hat_satellite/#{ForemanThemeSatellite.documentation_version}/html-single/administering_red_hat_satellite/index#configuring-satellite-server-for-cloud-connection"
+  end
+
   def test_docs_redirect_unknown_chapter
     get "/links/docs/Managing_Hosts"
 
